1cba8e7dcbf3ba7c21ad0a28ce343526b6e56193
Crossoufire <vince.delmas.38@gmail.com>
2026-06-05 19:56:18 +0200

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -5,6 +5,29 @@ import {formattedErrorAdapter, formZodErrorAdapter, unauthorizedErrorAdapter} fr
 
 const csrfMiddleware = createCsrfMiddleware({
     filter: (ctx) => ctx.handlerType === "serverFn",
+    origin: (origin, ctx) => {
+        /**
+         * Necessary for old (safari) browsers that only send `Referer` and not `Sec-Fetch-Site` or `Origin` (on GET at least),
+         * but because the app is behind http nginx proxy (https handled by cloudflare) then orgin is http instead of https
+         * and so csrf middleware returns 403 Forbidden. For newer browsers that then `Sec-Fetch-Site` no problems.
+         * Necessary to add:
+         *
+         *      proxy_set_header X-Forwarded-Host $host;
+         *      proxy_set_header X-Forwarded-Proto https;
+         *
+         * in the nginx conf file. If not using Cloudflare but nginx with certbot locally, (so https is handled by nginx) this
+         * is normally not needed.
+         */
+
+        const forwardedProto = ctx.request.headers.get("X-Forwarded-Proto");
+        const forwardedHost = ctx.request.headers.get("X-Forwarded-Host") ?? ctx.request.headers.get("Host");
+
+        if (forwardedProto && forwardedHost) {
+            return origin === `${forwardedProto}://${forwardedHost}`;
+        }
+
+        return origin === new URL(ctx.request.url).origin;
+    },
     failureResponse: (ctx) => {
         const requestUrl = new URL(ctx.request.url);
 
